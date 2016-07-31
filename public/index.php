450a718872742e436f34c22f7f7ff3da3d095cfb
Paul Welty <paul@synaxisworks.com>
2016-07-31 11:22:06 -0400

    adding quick replies to test

--- public/index.php
+++ public/index.php
@@ -42,6 +42,16 @@ switch ($_SERVER['REQUEST_METHOD']) {
     $person = getUserProfile($senderId);
     $replyText = $message->text.' received, '.$person->first_name;
     sendMessage($senderId,$replyText);
+    $replyOption = new stdClass;
+    $replyOption->content_type = "text";
+    $replyOption->title = "Option 1";
+    $replyOption->payload = "Option 1";
+    $replyOptions[]=$replyOption;
+    $replyOption->content_type = "text";
+    $replyOption->title = "Option 2";
+    $replyOption->payload = "Option 2";
+    $replyOptions[]=$replyOption;
+    sendQuickReply($senderId,"Pick something",$replyOptions)
     break;
 }
 
@@ -53,11 +63,24 @@ function sendMessage($recipientId,$text) {
 }
 
 function getUserProfile($userId) {
-  // https://graph.facebook.com/v2.6/<USER_ID>?access_token=PAGE_ACCESS_TOKEN
+  // "first_name": "Peter",
+  // "last_name": "Chang",
+  // "profile_pic": "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xpf1/v/t1.0-1/p200x200/13055603_10105219398495383_8237637584159975445_n.jpg?oh=1d241d4b6d4dac50eaf9bb73288ea192&oe=57AF5C03&__gda__=1470213755_ab17c8c8e3a0a447fed3f272fa2179ce",
+  // "locale": "en_US",
+  // "timezone": -7,
+  // "gender": "male"
   $url = 'https://graph.facebook.com/v2.6/'.$userId.'?access_token='.getenv('PAGE_ACCESS_TOKEN');
   $response = file_get_contents($url);
   $person = json_decode($response);
   return $person;
+}
+
+function sendQuickReply($recipientId,$text,$quickReplies) {
+  $sendArray = array();
+  $sendArray['recipient']['id']=$recipientId;
+  $sendArray['message']['text']=$text;
+  $sendArray['message']['quick_replies']=$quickReplies;
+  return postSomething($sendArray);
 }
 
 function postSomething($messageData) {
