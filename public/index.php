b8e8d210a708df3f777d1d9d8fa0a657d43e80e1
Paul Welty <paul@synaxisworks.com>
2016-07-31 09:02:55 -0400

    basic fb verify

--- public/index.php
+++ public/index.php
@@ -2,9 +2,23 @@
 
 require('../vendor/autoload.php');
 
-echo "running";
-error_log("hello, this is a test!");
+// echo "running";
+// error_log("hello, this is a test!");
 $x = print_r($_GET,true);
 error_log($x);
+
+$hub_mode = $_GET['hub.mode'];
+$hub_challenge = $_GET['hub.challenge'];
+$hub_verify_token = $_GET['hub.verify_token'];
+
+switch ($hub_mode) {
+  case 'subscribe':
+    error_log ("Subscribe");
+    if ($hub_verify_token==getenv('VERIFY_TOKEN')) {
+      echo($hub_challenge);
+    }
+    break;
+
+}
 
 ?>
